7a88918536424f58d2771805eac1d6c2a3106e84
mariokats98 <83875369+mariokats98@users.noreply.github.com>
2025-09-07 12:11:10 -0400

    Update page.tsx

--- app/edgar/page.tsx
+++ app/edgar/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import { useEffect, useMemo, useRef, useState } from "react";
-import tickerMap from "./data/tickerMap.json";
+import { useState } from "react";
+import tickerMap from "../data/tickerMap.json";  
+
 
 type Filing = {
   cik: string;
