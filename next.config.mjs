9451862cdaa1008b3d15f2ce0a851939433a872d
mariokats98 <83875369+mariokats98@users.noreply.github.com>
2025-09-28 16:55:43 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+
+  async redirects() {
+    return [
+      {
+        source: '/screener/stocks',
+        destination: '/<YOUR-STOCKS-PATH>', // e.g. '/stocks'
+        permanent: false,
+      },
+      {
+        source: '/screener/insider-activity',
+        destination: '/<YOUR-INSIDER-PATH>', // e.g. '/insider'
+        permanent: false,
+      },
+      {
+        source: '/screener/crypto',
+        destination: '/<YOUR-CRYPTO-PATH>', // e.g. '/crypto'
+        permanent: false,
+      },
+      {
+        source: '/screener/forex',
+        destination: '/<YOUR-FOREX-PATH>', // e.g. '/forex'
+        permanent: false,
+      },
+    ];
+  },
 };
+
 export default nextConfig;
