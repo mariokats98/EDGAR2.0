59874c0d01064ab340b8e092a8c666e0624ae298
mariokats98 <83875369+mariokats98@users.noreply.github.com>
2025-09-28 18:02:38 -0400

    Update InsiderTape.tsx

--- app/components/InsiderTape.tsx
+++ app/components/InsiderTape.tsx
@@ -3,8 +3,6 @@
 import { useEffect, useMemo, useState } from "react";
 // (rest unchanged)
 
-"use client";
-
 import { useEffect, useMemo, useState } from "react";
 
 export type TxnFilter = "ALL" | "A" | "D";
