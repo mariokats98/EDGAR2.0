f66183fb788e42aadea8f0b587a0d536b7daf29e
mariokats98 <83875369+mariokats98@users.noreply.github.com>
2025-10-20 19:28:09 -0400

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,7 +9,7 @@ declare global {
 export const prisma =
   global.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"]
+    log: process.env.NODE_ENV === "development" ? ["query", "warn", "error"] : ["error"],
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
