f3a25a23bf6c75143d05866615f3e2c6abdb7f96
mariokats98 <83875369+mariokats98@users.noreply.github.com>
2025-10-20 20:05:19 -0400

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,10 +6,16 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+// Reuse the Prisma instance on hot reloads in dev. New one in prod.
 export const prisma =
   global.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "warn", "error"] : ["error"],
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
+}
