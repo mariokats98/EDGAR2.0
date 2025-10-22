405f40b2f500a7b74e7be918cec6384a41e9cb4e
mariokats98 <83875369+mariokats98@users.noreply.github.com>
2025-10-22 18:13:00 -0400

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,5 @@
-// app/api/auth/[...nextauth]/route.ts
-import NextAuth from 'next-auth';
-import { authOptions } from '@/lib/auth';
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
 
