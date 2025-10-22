53956207bf66ce414b9667d2f6c5127e62f410fd
mariokats98 <83875369+mariokats98@users.noreply.github.com>
2025-10-22 17:40:36 -0400

    Create next-auth.d.ts

---
+++ types/next-auth.d.ts
@@ -0,0 +1,20 @@
+// types/next-auth.d.ts
+import NextAuth, { DefaultSession } from "next-auth";
+import { JWT as DefaultJWT } from "next-auth/jwt";
+import type { Role } from "@prisma/client";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      role: Role;
+    } & DefaultSession["user"];
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT extends DefaultJWT {
+    id: string;
+    role: Role;
+  }
+}
