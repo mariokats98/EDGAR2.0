6074ae7c4a97a493eec3b20dd2997f73b0566a54
mariokats98 <83875369+mariokats98@users.noreply.github.com>
2025-10-22 17:32:08 -0400

    Update prisma.ts

--- prisma/lib/prisma.ts
+++ prisma/lib/prisma.ts
@@ -1,12 +1,11 @@
-// lib/prisma.ts
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient }
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ["warn", "error"],
-  });
+  })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
